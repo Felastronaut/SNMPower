ffff9b73ace34eaf9567fa2c51677c23d0643e69
Felastronaut <felix.meynet@master-stic.fr>
2020-11-29 14:50:23 +0100

    update api calls

---
+++ Agent/api.sh
@@ -0,0 +1,9 @@
+curl --location --request POST 'http://localhost:3000/posts' \
+--header 'Authorization: 3b036afe-0110-4202-b9ed-99718476c2e0' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+      "id": 2,
+      "cpu": "13",
+      "ram": "1024"
+      "bit rate interface 1" : "122"
+}'
